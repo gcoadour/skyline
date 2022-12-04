a6e7967f56eff24c21610499533652ffcfaa425a
gcoadour <47463874+gcoadour@users.noreply.github.com>
2022-12-04 12:55:38 +0100

    Update ICommonStateGetter.cpp

--- app/src/main/cpp/skyline/services/am/controller/ICommonStateGetter.cpp
+++ app/src/main/cpp/skyline/services/am/controller/ICommonStateGetter.cpp
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: MPL-2.0 a
 // Copyright © 2020 Skyline Team and Contributors (https://github.com/skyline-emu/)
 
 #include <common/settings.h>
